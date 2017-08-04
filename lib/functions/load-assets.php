5fee6070ad4d090c40b2be13b1a14ca5f46a964b
jeseph <jeseph@qcmny.com>
2017-08-03 20:57:53 -0400

    lint styles

--- lib/functions/load-assets.php
+++ lib/functions/load-assets.php
@@ -38,11 +38,11 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
  */
 function enqueue_assets() {
 
-	wp_enqueue_style( CHILD_TEXT_DOMAIN . '-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	// wp_enqueue_style( CHILD_TEXT_DOMAIN . '-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
 
 	wp_enqueue_script( 'genesis-sample-responsive-menu', CHILD_URL . '/assets/js/responsive-menu.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
-	
+
     $localized_script_args = array(
 		'mainMenu' => __( 'Menu', CHILD_TEXT_DOMAIN ),
 		'subMenu'  => __( 'Menu', CHILD_TEXT_DOMAIN ),
